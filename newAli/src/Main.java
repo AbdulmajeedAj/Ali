56828557b916ba21755628a2b4bcaee7a18258fd
AbdulmajeedAj <152404161+AbdulmajeedAj@users.noreply.github.com>
2023-11-29 21:05:06 +0300

    Add files via upload

---
+++ newAli/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Zawardo");
+    }
+
+    static int add(int n1, int n2) {
+        return 0;
+
+    }
+}
+
